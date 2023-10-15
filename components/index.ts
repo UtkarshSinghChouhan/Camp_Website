2026c8923b779785d2f5f9e3bcb70c1dd128c89a
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-10-15 19:02:47 +0530

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,22 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Button from "./Button";
+
+import Hero from "./Hero";
+import Camp from "./Camp";
+import Features from "./Features";
+import GetApp from "./GetApp";
+import Guide from "./Guide";
+import FeatureItem from "./FeatureItem";
+
+export {
+  Navbar,
+  Footer,
+  Button,
+  Hero,
+  Camp,
+  Features,
+  GetApp,
+  Guide,
+  FeatureItem,
+};
